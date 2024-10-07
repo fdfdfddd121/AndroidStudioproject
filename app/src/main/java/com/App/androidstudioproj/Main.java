c3c5a1af96f3a657a3da1dde4676d60a2a8381ab
User <nitaykaraso15@gmail.com>
2024-10-07 22:17:22 +0300

    home work

--- app/src/main/java/com/App/androidstudioproj/Main.java
+++ app/src/main/java/com/App/androidstudioproj/Main.java
@@ -1,8 +1,10 @@
 package com.App.androidstudioproj;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +12,82 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Main extends AppCompatActivity {
     TextView textView;
-    Button button;
+    Button namebutton;
+    Button phonebutton;;
+    Button emailbutton;
+    Button redbutton;;
+    Button greenbutton;
+    Button bluebutton;
+    EditText name;
+    EditText phone;
+    EditText email;
+    EditText red;
+    EditText green;
+    EditText blue;
+    int color;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=(TextView) findViewById(R.id.text_id);
-        button=(Button) findViewById(R.id.button_id);
-        button.setOnClickListener(new View.OnClickListener() {
-            int count = 0;
+        namebutton=(Button) findViewById(R.id.namebutton);
+        phonebutton=(Button) findViewById(R.id.phonebutton);
+        emailbutton=(Button) findViewById(R.id.emailbutton);
+        redbutton=(Button) findViewById(R.id.redbutton);
+        greenbutton=(Button) findViewById(R.id.greenbutton);
+        bluebutton=(Button) findViewById(R.id.bluebutton);
+        name=(EditText) findViewById(R.id.nametext);
+        phone=(EditText) findViewById(R.id.phonetext);
+        email=(EditText) findViewById(R.id.emailtext);
+        red=(EditText) findViewById(R.id.redtext);
+        green=(EditText) findViewById(R.id.greentext);
+        blue=(EditText) findViewById(R.id.bluetext);
+        
+        color = textView.getCurrentTextColor();
+
+
+        namebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count++;
-                if(count%2==1) {
-                    textView.setText("you changed me");
-                }
-                else {
-                    textView.setText("some random title");
-                }
+                textView.setText(name.getText().toString());
             }
         });
+        phonebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(phone.getText().toString());
+            }
+        });
+        emailbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(email.getText().toString());
+            }
+        });
+        redbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newColor = Color.argb(Color.alpha(color), Integer.parseInt(red.getText().toString()), Color.green(color), Color.blue(color));
+                color = newColor;
+                textView.setTextColor(newColor);
+            }
+        });
+        greenbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newColor = Color.argb(Color.alpha(color), Color.red(color), Integer.parseInt(green.getText().toString()), Color.blue(color));
+                color = newColor;
+                textView.setTextColor(newColor);
+            }
+        });
+        bluebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newColor = Color.argb(Color.alpha(color), Color.red(color), Color.green(color), Integer.parseInt(blue.getText().toString()));
+                color = newColor;
+                textView.setTextColor(newColor);
+            }
+        });
+
     }
 }
